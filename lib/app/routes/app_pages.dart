96aadd82e99605036ef26974203f393b2efab219
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-12-26 15:56:25 +0600

    Final Touch

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,11 +1,12 @@
 import 'package:get/get.dart';
+import 'package:homenest/app/routes/app_routes.dart';
 import '../../presentation/home/views/home_view.dart';
 import '../../presentation/home/controllers/home_controller.dart';
 
 class AppPages {
   static final pages = [
     GetPage(
-      name: '/home',
+      name: AppRoutes.HOME,
       page: () => const HomeView(),
       binding: BindingsBuilder(() {
         Get.lazyPut<HomeController>(() => HomeController());
